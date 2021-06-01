cd73b6b2dde94c25b967fd99e2cc547ca6926ec2
Suha Shehadeh <64090569+Suha523@users.noreply.github.com>
2021-06-01 16:43:38 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // pathPrefix: "/Zaat.dev-Blog",
+  pathPrefix: "/Zaat.dev-Blog",
   siteMetadata: {
     title: `Zaat.dev Blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
